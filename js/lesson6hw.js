c1dcf919770a96e63fb0e3e4f399804f27f195b2
ShannonMiller <shans.miller@gmail.com>
2015-08-13 17:10:47 -0400

    added sun and boat images and wrote javas code

--- js/lesson6hw.js
+++ js/lesson6hw.js
@@ -1,4 +1,20 @@
 $(document).ready(function(){
-	alert("Welcome to Lesson 6 Home Work")
+	alert("Welcome to Lesson 6 Home Work");
 
-});
+	$("#curtain").animate({ top:"-=900px"
+	},3000);
+
+	$("#boat").click(function(){
+		// moveBoat();
+		$(this).animate({
+			marginLeft: "400px"
+		}, 2000)
+	})
+  });
+
+ 
+	
+
+
+			
+
