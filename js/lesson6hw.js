894ade8ac65f1dcb2acf36ff14a77b596993b221
ShannonMiller <shans.miller@gmail.com>
2015-08-13 17:36:27 -0400

    added java to sun and boat

--- js/lesson6hw.js
+++ js/lesson6hw.js
@@ -1,14 +1,30 @@
 $(document).ready(function(){
-	alert("Welcome to Lesson 6 Home Work");
+	//alert("Welcome to Lesson 6 Home Work");
 
 	$("#curtain").animate({ top:"-=900px"
 	},3000);
 
+	setTimeout(function(){
+		$(".click-boat").fadeIn();
+
+	},4000)
+
 	$("#boat").click(function(){
-		// moveBoat();
+		$(".click-boat").fadeOut();
 		$(this).animate({
 			marginLeft: "400px"
-		}, 2000)
+		}, 2000);
+		$(".click-sun").fadeIn(2000);
+	})
+	// setTimeout(function(){
+	// 	$(".click-sun").fadeIn();
+
+	// },4000)
+
+	$("#sun").click(function(){
+		$("#curtain").animate({ top:"+=900px"
+	},3000);
+
 	})
   });
 
