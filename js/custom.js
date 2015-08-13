4ac0e9f24e4b1f805176a70383bae0813e11ebf7
ShannonMiller <shans.miller@gmail.com>
2015-08-13 09:04:33 -0400

    finished ball animation added new html and js file for social media animation

--- js/custom.js
+++ js/custom.js
@@ -5,17 +5,32 @@ $(document).ready(function(){
 	$("#lightSwitch").click(function(){
 		flipTheLightSwitch();
 	});
+	//event listener makes the ball roll
+	 $("#ball").click(function(){
+	 	var windowWidth = $(window).width();
 
+		console.log(windowWidth)
+		console.log( $("#ball").position() )
 
+		if($("#ball").position() <= windowWidth ){
+	 		$("#ball").animate( {left:"+=100px"} );
+	 	}else{
+	 		$("#ball").animate( {left:"-=100px"} );
+	 	}
+	 });
+
+	 //light switch example turning something on and off
 	function flipTheLightSwitch(){
 		if(isLightOn){
 			
 			$("body").css("background", "black");
 			$("#lightSwitch").val("Turn Light On")
+			$("#square").fadeOut(3000);
 			isLightOn = false;
 		} else{
 			$("body").css("background", "yellow");
 			$("#lightSwitch").val("Turn Light Off")
+			$("#square").fadeIn(3000);
 			isLightOn = true;
 			}
 
